110c83e3f81dac8ee86f3e6fdd745c46ada76545
AnToR6660 <110006343+AnToR6660@users.noreply.github.com>
2023-08-18 01:14:30 +0600

    Add files via upload

---
+++ labtask6/view/UserProfile/UserProfile.php
@@ -0,0 +1,39 @@
+
+<?php
+        session_start();
+        
+        while(isset($_SESSION['username']))
+        {
+            require'../navbar2.php';
+            
+            
+            echo"hello ".$_SESSION['username'];
+            echo"<br>";
+            
+    ?>
+    <html>
+    <title>
+
+    </title>
+    <head>
+
+    </head>
+    <body>
+        <a href="viewprofile.php">view profile</a><br>
+        <a href="editprofile.php">edit profile</a><br>
+        <a href="changepass.php">change password</a><br>
+
+    </body>
+    <?php
+        break;}  
+    ?>
+    <?php
+        while(!isset($_SESSION['username']))
+        {
+            require'../navbar.php';
+            echo "youre not logged in";
+            break;
+        }
+        ?>
+
+        
